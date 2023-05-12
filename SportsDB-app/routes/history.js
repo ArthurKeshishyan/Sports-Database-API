2f0b66d7796e99fc143d5d378b3e1709328c5d27
AceArthur10 <akeshis2@calstatela.edu>
2023-05-11 17:20:33 -0700

    third commit

--- SportsDB-app/routes/history.js
+++ SportsDB-app/routes/history.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const { MongoClient } = require('mongodb');
 const config = require('../config.json');
 
+
 const mongoURL = `mongodb+srv://${config.username}:${config.password}@cluster0.jhsbzxg.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 router.get('/', async (req, res) => {
     try {
@@ -12,14 +14,19 @@ router.get('/', async (req, res) => {
         const db = client.db(config.database_name);
         const historyCollection = db.collection('history');
 
+
         let history;
         if (req.query.searchTerm) {
             history = await historyCollection.find({ searchTerm: req.query.searchTerm }).toArray();
+            if (history.length === 0) {
+                return res.status(404).json({ error: "not found" });
+            } else {
+                return res.json(history);
+            }
         } else {
             history = await historyCollection.find({}).toArray();
+            return res.json(history);
         }
-
-        res.json(history);
     } catch (err) {
         console.error(err);
         res.status(500).send('Internal server error');
@@ -28,4 +35,8 @@ router.get('/', async (req, res) => {
     }
 });
 
+
 module.exports = router;
+
+
+
